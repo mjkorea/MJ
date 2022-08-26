54980889d641f73bcb5fe4f59db910675f7f37b6
mjkorea <111555904+mjkorea@users.noreply.github.com>
2022-08-26 14:04:27 +0900

    Add files via upload

---
+++ while 연습-5.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+main()
+{
+/*	int i = 10;
+	
+	do {
+		printf("%d ", i);
+		i--;
+	}while(i>=5);
+	
+	printf("test");
+	
+	printf("\n=============================================================\n");*/
+	
+/*	int i, n;
+	i = 0;
+	printf("정수 : ");
+	scanf("%d", &n) ;
+	do{
+		printf(" I love programming\n");
+		i++;
+	}while(i < n);
+	
+	printf("\n예제 12============================================================\n");*/
+	
+	int num;
+	do
+/*	{
+		printf("숫자 입력 : ");
+		scanf("%d", &num);
+		
+		if(num>0){
+			if(num%2 == 1)
+				printf("홀수\n");
+			else if(num%2 == 0)
+			printf("짝수\n");
+		} 
+	}while(num>0);*/
+	
+/*	int i ,sum = 0;
+	i=1;
+	do{
+		sum = sum + i;
+		i = i + 2;
+	}while(i<=50);
+	printf("1부터 50까지의 홀수의 합 : %d", sum);*/
+}
---
+++ while 연습-6.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+main()
+{
+/*	printf("\n예제14=============================\n");
+	int n, sum=0;
+	do{
+	
+		scanf("%d" , &n);
+		sum = sum + n;
+	}while(n!=0);
+	printf("합 : %d",sum);
+	
+	int i = 0, num = 0;
+	*/
+/*	int i, num;
+	i = 1;
+	printf("약수를 구할 수 : ");
+	scanf("%d", &num);
+	 
+	while(i<=8){
+		if(num%i==0)
+			printf("%d ", i);
+		i++;
+	}*/
+/*	int su, cnt = 0, i;
+	
+	printf("숫자입력 : ");
+	scanf("%d", &su);
+	
+	i = 1;
+	while(i <= su)
+	{if(su%i==0)
+	cnt++;
+	i++;
+	 } 
+	 if(cnt==2)
+	 printf("%d은(는) 소수\n", su);
+	 else
+	 printf("%d은(는) 소수가 아님", su);*/
+	 
+	 
+}
